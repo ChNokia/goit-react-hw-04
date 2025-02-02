566e0a8edd1c1c5b5d2866b9e2ca7e9d2e8ae460
Sergiy Chyruk <sergii.chyruk@dxc.com>
2025-02-02 14:06:04 +0200

    change api key vercel

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const fetchImages = async (query, page = 0) => {
   const response = await axios.get(
     `https://api.unsplash.com/search/photos?query=${query}&&page=${page}&client_id=${
-      import.meta.env.VITE_API_KEY
+      import.meta.env.VERCEL_API_KEY
     }`
   );
 
