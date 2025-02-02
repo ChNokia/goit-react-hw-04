32bff62d02c45e2bd9a2d7097ce4a26865eeadf1
Sergiy Chyruk <sergii.chyruk@dxc.com>
2025-02-02 14:44:04 +0200

    change api key test

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export const fetchImages = async (query, page = 0) => {
-  console.log(import.meta.env.VITE_API_KEY);
+  console.log(import.meta.env.VITE_API_ID);
   console.log(import.meta.env.VERCEL_API_KEY);
   console.log(import.meta.env.REACT_API_KEY);
   const response = await axios.get(
     `https://api.unsplash.com/search/photos?query=${query}&&page=${page}&client_id=${
-      import.meta.env.VITE_API_KEY
+      import.meta.env.VITE_API_ID
     }`
   );
 
